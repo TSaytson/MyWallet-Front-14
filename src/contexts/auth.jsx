947f4e840b4cef8618b623b96866ddf590c519a9
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-01-28 18:03:35 -0300

    Deploy URL

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -6,8 +6,8 @@ export default function AuthProvider({children}){
     
     const [token, setToken] = useState(null);
     const [name, setName] = useState('');
-    const REACT_APP_API_URL = 'http://localhost:5000';
-    // const REACT_APP_API_URL = 'https://mywallet-api-jy4z.onrender.com';
+    // const REACT_APP_API_URL = 'http://localhost:5000';
+    const REACT_APP_API_URL = 'https://mywallet-api-jy4z.onrender.com';
     
     return(
         <AuthContext.Provider value={ { name, setName, token, setToken, REACT_APP_API_URL } } >
